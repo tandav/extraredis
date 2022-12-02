31a766eee2c984e8b86fce4e3e1dc5fb477ae83c
Alexander Rodionov <tandav@tandav.me>
2022-12-02 14:29:53 +0300

    wip

--- setup.py
+++ setup.py
@@ -1,3 +1,27 @@
+import unasync
 from setuptools import setup
 
-setup()
+
+ADDITIONAL_REPLACEMENTS = {
+    'redis_asyncio': 'redis_sync',
+    'fake_redis_async': 'fake_redis_sync',
+    'pytest_mark_asyncio': 'pytest_mark_sync',
+    'pytest_asyncio': 'pytest',
+    'ExtraRedisAsync': 'ExtraRedis',
+}
+
+
+setup(
+    cmdclass={'build_py': unasync.cmdclass_build_py(rules=[
+        unasync.Rule(
+            fromdir='/extraredis/_async/',
+            todir='/extraredis/_sync/',
+            additional_replacements=ADDITIONAL_REPLACEMENTS,
+        ),
+        unasync.Rule(
+            fromdir='/tests/_async/',
+            todir='/tests/_sync/',
+            additional_replacements=ADDITIONAL_REPLACEMENTS,
+        ),
+    ])},
+)
