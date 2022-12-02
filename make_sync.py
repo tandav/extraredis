969039c3d62067f616b00c7dadb69505d3a1acf1
Alexander Rodionov <tandav@tandav.me>
2022-12-02 14:40:39 +0300

    wip

--- make_sync.py
+++ make_sync.py
@@ -1,29 +1,41 @@
+import os
+from pathlib import Path
 
-# import unasync
+import unasync
 
-# ADDITIONAL_REPLACEMENTS = {
-#     'redis_asyncio': 'redis_sync',
-#     'fake_redis_async': 'fake_redis_sync',
-#     'pytest_mark_asyncio': 'pytest_mark_sync',
-#     'pytest_asyncio': 'pytest',
-#     'ExtraRedisAsync': 'ExtraRedis',
-# }
-
-
-# def main():
-#     rules = [
-#         unasync.Rule(
-#             fromdir='/extraredis/_async/',
-#             todir='/extraredis/_sync/',
-#             additional_replacements=ADDITIONAL_REPLACEMENTS,
-#         ),
-#         unasync.Rule(
-#             fromdir='/tests/_async/',
-#             todir='/tests/_sync/',
-#             additional_replacements=ADDITIONAL_REPLACEMENTS,
-#         ),
-#     ]
+ADDITIONAL_REPLACEMENTS = {
+    'redis_asyncio': 'redis_sync',
+    'fake_redis_async': 'fake_redis_sync',
+    'pytest_mark_asyncio': 'pytest_mark_sync',
+    'pytest_asyncio': 'pytest',
+    'ExtraRedisAsync': 'ExtraRedis',
+}
 
 
-# if __name__ == '__main__':
-#     main()
+def main():
+    rules = [
+        unasync.Rule(
+            fromdir='/extraredis/_async/',
+            todir='/extraredis/_sync/',
+            additional_replacements=ADDITIONAL_REPLACEMENTS,
+        ),
+        unasync.Rule(
+            fromdir='/tests/_async/',
+            todir='/tests/_sync/',
+            additional_replacements=ADDITIONAL_REPLACEMENTS,
+        ),
+    ]
+    filepaths = []
+    for root, _, filenames in os.walk(Path(__file__).absolute().parent):
+        for filename in filenames:
+            if filename.rpartition('.')[-1] in (
+                'py',
+                'pyi',
+            ):
+                filepaths.append(os.path.join(root, filename))
+
+    unasync.unasync_files(filepaths, rules)
+
+
+if __name__ == '__main__':
+    main()
